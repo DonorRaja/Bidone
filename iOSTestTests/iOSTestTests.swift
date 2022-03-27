a8ea648b96333f4104d2272b0b3f0abfb77f473b
DonorRaja <srk.raja4@gmail.com>
2022-03-28 03:13:06 +1300

    unit test cases implementation.

--- iOSTestTests/iOSTestTests.swift
+++ iOSTestTests/iOSTestTests.swift
@@ -9,18 +9,35 @@ import XCTest
 @testable import iOSTest
 
 class iOSTestTests: XCTestCase {
-
+    
+   
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
     }
 
+    func CalculateGST() throws {
+        let gst = Utility.calculateGST(excludeGST: 250, percentage: 15)
+        XCTAssertEqual(37.5, gst)
+        
+    }
+    
+    func currencyFormatCheck() throws {
+        let currncy = Utility.makeCurrencyFormatter(currentAmount: 566.98765)
+        print(currncy)
+        XCTAssertEqual( "$566.99", currncy)
+    }
+    
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+       
     }
 
     func testPerformanceExample() throws {
